377e8a8c1207b61d9065a62bba3628793d0d9395
MateoRancanoMartinez <mateo.arganza@gmail.com>
2025-04-08 12:37:42 +0200

    delete

--- Assignment1-Dynamics/data/read_txt_matlab.m
+++
@@ -1,6 +0,0 @@
-
-t = load('t.txt'); % epochs - s
-
-r = load('r.txt'); % precise positions - km
-
-v = load('v.txt'); % precise velocities - km/s
--- Assignment1-Dynamics/data/read_txt_python.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-t = np.loadtxt('t.txt') # epochs - s
-
-r = np.loadtxt('r.txt') # precise positions - km
-
-v = np.loadtxt('v.txt') # precise velocities - km/s
-
