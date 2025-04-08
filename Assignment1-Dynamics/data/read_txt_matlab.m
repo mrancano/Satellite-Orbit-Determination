e079f69e47f3b07b863b9e46c09a971c0046d342
MateoRancanoMartinez <mateo.arganza@gmail.com>
2025-04-08 12:23:39 +0200

    Assignment 1 - Dynamics

---
+++ Assignment1-Dynamics/data/read_txt_matlab.m
@@ -0,0 +1,6 @@
+
+t = load('t.txt'); % epochs - s
+
+r = load('r.txt'); % precise positions - km
+
+v = load('v.txt'); % precise velocities - km/s
---
+++ Assignment1-Dynamics/data/read_txt_python.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+t = np.loadtxt('t.txt') # epochs - s
+
+r = np.loadtxt('r.txt') # precise positions - km
+
+v = np.loadtxt('v.txt') # precise velocities - km/s
+
